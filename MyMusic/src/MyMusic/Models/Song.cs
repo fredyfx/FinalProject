ee5032c43bf385f9432d9cfbe7e0182a7c29551d
gcguevara <garrettcguevara@gmail.com>
2017-01-31 16:42:46 -0800

    Added input validation to Song.

--- MyMusic/src/MyMusic/Models/Song.cs
+++ MyMusic/src/MyMusic/Models/Song.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,23 @@ namespace MyMusic.Models
     public class Song
     {
         public int ID { get; set; }
+
+        // add validation with built in StringLength and Range
+        // these return a built in validation message with client side Jquery
+
+        [StringLength(60, MinimumLength = 1)]
         public string Title { get; set; }
+
+        [StringLength(60, MinimumLength = 1)]
         public string Artist { get; set; }
+
+        [StringLength(60, MinimumLength = 1)]
         public string Album { get; set; }
+
+        [Range(0, 2100)]
         public int Year { get; set; }
+
+        [StringLength(60, MinimumLength = 1)]
         public string Genre { get; set; }
     }
 }
